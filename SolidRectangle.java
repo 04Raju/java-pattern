311daa97d94db132ac8b9bbbe93a7969b2b8dafc
Raju <Rajujettappascms@gmal.com>
2021-09-27 23:02:28 +0530

    java Patterns

---
+++ SolidRectangle.java
@@ -0,0 +1,18 @@
+package com.patterns;
+
+public class SolidRectangle {
+
+	
+	public static void main(String[] args) {
+		//outerloop
+		for(int i=1; i<=5; i++) {
+			//inner loop
+			for(int j=1;j<=5;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+}
